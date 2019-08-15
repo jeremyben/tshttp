0db79acbe73a9def220a4c12a011b9de84a74ec1
jeremyben <bensimon.jeremy@gmail.com>
2019-08-15 23:28:16 +0200

    feat(status): http status codes as union types

--- status/index.ts
+++ status/index.ts
@@ -2,3 +2,5 @@ import { Status } from './status'
 export default Status
 
 export { InformationStatus, SuccessStatus, RedirectionStatus, ErrorStatus } from './status'
+
+export { InformationStatusU, SuccessStatusU, RedirectionStatusU, ErrorStatusU, StatusU } from './union'
